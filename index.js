48927a3d39ff54bffdac447ebf06106c964de727
Shinkay <shinkayb@gmail.com>
2018-05-01 23:15:36 -0400

    need to be fixed

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-var randomCharacter = require('./game.js');
-var wordCons = require('./Word.js');
-var letterCons = require('./Letter.js');
 var inquirer = require('inquirer');
-var letterGuessed = 0;
+var isLetter = require('./Letter');
+var Word = require('./Word.js');
+var Game = require('./Game.js');
+var hangManDisplay = Game.newWord.hangman;
+
+var hangman = {
+    wordBank: Game.newWord.wordList,
+    guessesRemaining: 10,
+    guessedLetters: [],
+    display: 0,
+    currentWord: null,
+    startGame: function() {
+      var that = this;
+      if(this.guessedLetters.length > 0){
+        this.guessedLetters = [];
+      }
+    }
+}
