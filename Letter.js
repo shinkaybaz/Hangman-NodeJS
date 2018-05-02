52f228506787f293fddaa10e6ec9f74d379edfdd
Shinkay <shinkayb@gmail.com>
2018-05-01 23:08:53 -0400

    code code

---
+++ Letter.js
@@ -0,0 +1,46 @@
+class Letter {
+    constructor(character) {
+        this.character = character;
+        // this.appear = false;
+        if (this.character === '')
+            this.appear = true;
+        console.log(character);
+    }
+    printInfo() {
+        if (this.appear) {
+            return this.character;
+        }
+        return ' _ ';
+    }
+}
+
+
+
+// var letter = new Letter(this.character);
+// letter.printInfo();
+
+module.exports = Letter;
+
+
+var Letter = function(ltr) {
+    // property to store the actual letter
+      this.letter = ltr;
+    // property/boolean if the letter can be shown
+      this.appear = false;
+    
+      this.letterRender = function() {
+        if(this.letter == ' '){ /*renders a blank as it is*/
+          //makes sure that when the function checks if the word is found doesn't read the blank as false.
+          this.appear = true;
+          return '  ';
+        }if(this.appear === false){ /*if it doesn't appear, it returns a ' _ '*/
+          return ' _ ';
+        } else{ /*otherwise it just appears as itself*/
+          return this.letter;
+        }
+    
+      };
+    };
+    
+    // export to use in word.js
+    module.exports = Letter;
---
+++ index.js
@@ -0,0 +1,5 @@
+var randomCharacter = require('./game.js');
+var wordCons = require('./Word.js');
+var letterCons = require('./Letter.js');
+var inquirer = require('inquirer');
+var letterGuessed = 0;
